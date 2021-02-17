ca1d3fc7d92fa905c04351768f141010b09b141e
Simon <39030048+tidders2000@users.noreply.github.com>
2021-02-17 10:54:06 +0000

    Delete env.py

--- env.py
+++
@@ -1,9 +0,0 @@
-import os
-os.environ.setdefault(
-    "SECRET_KEY", '_u1tp%4-5_s8w1cf2i#4jqe5$!c=h#2hu-z5-t)xm&3af9xi%8')
-
-os.environ.setdefault(
-    "SOCIAL_AUTH_FACEBOOK_SECRET", "75c8465d4865f51d54aabf1b8e90b6a0")
-os.environ.setdefault(
-    "SOCIAL_AUTH_FACEBOOK_KEY", '450265435842966'
-)
